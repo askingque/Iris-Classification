897282553bf8769545f8afa32d0e567db341bfd2
[Noah King] <[noah.king@bsu.edu]>
2023-10-29 02:43:45 -0400

    Update irisModel.py

--- irisModel.py
+++ irisModel.py
@@ -1,5 +1,5 @@
 from keras import Sequential
-from keras.layers import Dense
+from keras.layers import Dense, Conv2D, Dropout, Flatten
 from keras.utils import to_categorical
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
@@ -13,6 +13,11 @@ X_test = test.iloc[:, :4]
 input_strings = train.iloc[:, 4]
 label_encoder = LabelEncoder()
 input_labels = label_encoder.fit_transform(input_strings)
+y_train = to_categorical(input_labels)
+
+output_strings = test.iloc[:, 4]
+output_labels = label_encoder.fit_transform(output_strings)
+y_test = to_categorical(output_labels)
 
 #pre-process data, helps with speed I guess :P
     #create standard deviation of 1
@@ -24,20 +29,24 @@ mean = np.mean(X_train)
 X_train -= mean
 X_test -= mean
 
-y_train = to_categorical(input_labels)
+
 
 #input = [1, 4]
 #output = [1, 3]
+print(X_train.shape)
 
 model = Sequential()
-model.add(Dense(4, input_shape=(4,), activation='relu'))
-model.add(Dense(8, activation='relu'))
+model.add(Dense(64, activation='relu', input_shape=(4,)))
+model.add(Dropout(0.1))
+model.add(Dense(32, activation='relu'))
+model.add(Dropout(0.1))
+model.add(Dense(16, activation='swish'))
 model.add(Dense(3, activation='softmax'))
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(X_train, y_train, epochs=100)
+model.fit(X_train, y_train, epochs=1000)
 
-model.evaluate(X_test, y_test)
+print(model.evaluate(X_test, y_test))
 
 
 
